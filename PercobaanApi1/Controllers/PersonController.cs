99ea56e904413a933ef6d938e520e8b7e31f9fd0
Achmad Azriel Amaldany <117701194+AZR88@users.noreply.github.com>
2024-03-29 16:24:09 +0700

    Add files via upload
    JWT

---
+++ PercobaanApi1/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        private string __constr;
+
+        public PersonController(IConfiguration configuration)
+        {
+            __constr = configuration.GetConnectionString("WebApiDatabase");
+        }
+        
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        [HttpGet("API/PERSON")]
+
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext(this.__constr);
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+    }
+
+}
---
+++ PercobaanApi1/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿namespace PercobaanApi1.Models
+{
+    public class Login
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat {  get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string username { get; set; }
+        public int id_peran { get; set; }
+        public string nama_peran { get; set; }
+        public string token { get; set; }
+
+    }
+}
